79343db83f0e88a9cc160b8b329c54b8e8f29da9
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-03-02 22:39:55 +0300

    Update chernovik.cpp

--- chernovik.cpp
+++ chernovik.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sstream>
 
-class matrix_t{
+class matrix_t {
 private:
 	int ** data_;
 	unsigned int rows_;
@@ -15,46 +15,49 @@ public:
 		collumns_ = 0;
 	}
 
-	matrix_t(const unsigned int rows, const unsigned int collumns) {
-		rows_ = rows;
-		collumns_ = collumns;
-		
-		data_ = new int * [rows_];
+	matrix_t(matrix_t const & matrix) {
+		rows_ = matrix.rows_;
+		collumns_ = matrix.collumns_;
+
+		data_ = new int *[rows_];
 		for (unsigned int i = 0; i < rows_; ++i) {
 			data_[i] = new int[collumns_];
+			for (unsigned int j = 0; j < collumns_; ++j) {
+				data_[i][j] = matrix.data_[i][j];
+			}
 		}
 	}
 
 	matrix_t & operator = (matrix_t const & matrix) {
-		std::cout << "per" << std::endl;
 		if (this != &matrix) {
 			for (unsigned int i = 0; i < rows_; ++i) {
 				delete[] data_[i];
 			}
 			delete[] data_;
-			
+
 			rows_ = matrix.rows_;
-			std::cout << matrix.rows_ << std::endl;
 			collumns_ = matrix.collumns_;
-			data_ = new int * [rows_];
+			data_ = new int *[rows_];
 			for (unsigned int i = 0; i < rows_; ++i) {
 				data_[i] = new int[collumns_];
+			}
+			for (unsigned int i = 0; i < rows_; ++i) {
 				for (unsigned int j = 0; j < collumns_; ++j) {
 					data_[i][j] = matrix.data_[i][j];
 				}
 			}
 		}
 
-		return * this;
+		return *this;
 	}
 
 	matrix_t add(matrix_t const & other) const {
-		
+
 		if (rows_ == other.rows_ &&
 			collumns_ == other.collumns_) {
 
-			matrix_t result(rows_, collumns_);
-			
+			matrix_t result(other);
+
 			for (unsigned int i = 0; i < rows_; ++i) {
 				for (unsigned int j = 0; j < collumns_; ++j) {
 					result.data_[i][j] = data_[i][j] + other.data_[i][j];
@@ -67,8 +70,8 @@ public:
 			exit(0);
 		}
 	}
-  
-	std::ifstream & read( std::ifstream & stream, const std::string fileName ) {
+
+	std::ifstream & read(std::ifstream & stream, const std::string fileName) {
 		stream.open(fileName.c_str());
 
 		char symb;
@@ -90,7 +93,7 @@ public:
 		return stream;
 	}
 
-	std::ofstream & write( std::ofstream & stream, const std::string fileName ) const {
+	std::ofstream & write(std::ofstream & stream, const std::string fileName) const {
 		stream.open(fileName.c_str());
 
 		if (stream.is_open()) {
@@ -131,13 +134,13 @@ int main()
 
 	std::istringstream stroka(line);
 	std::string fileNameA;
-	
+
 	std::ifstream streamA;
-	
+
 	stroka >> fileNameA;
 	char symb;
 	if ((A.read(streamA, fileNameA)) && (stroka >> symb)) {
-		if (symb == '+' || symb == '-' || symb == '*' ) {
+		if (symb == '+' || symb == '-' || symb == '*') {
 			matrix_t B;
 			matrix_t R;
 
@@ -147,9 +150,9 @@ int main()
 			std::ofstream streamR;
 
 			if (B.read(streamB, fileNameB)) {
-	             
+
 				if (symb == '+') {
-					R = A.add(B); // вот здесь
+					R = A.add(B);
 					R.write(streamR, fileNameR);
 				}
 			}
@@ -166,9 +169,8 @@ int main()
 	else {
 		A.error();
 	}
-	
+
 	streamA.close();
 	std::cin.get();
-    return 0;
+	return 0;
 }
-
